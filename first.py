8bd6695009102662374a2b434067beb3bbe96ba7
Anthony Thomas La <anthony@eduroam-169-233-135-171.ucsc.edu>
2026-04-19 13:03:17 -0700

    added Loaded a pcap

--- first.py
+++ first.py
@@ -1,5 +1,15 @@
-from scapy.all import rdpcap
+from scapy.all import rdpcap, IP
+from datetime import datetime
 
 packets = rdpcap('app-norton-failed.pcapng')
 print(packets[0])
 print(packets[0].show())
+
+for pkt in packets:
+    ts = datetime.fromtimestamp(float(pkt.time))
+    proto = pkt.lastlayer().name
+
+    if IP in pkt:
+        src = pkt[IP].src
+        dst = pkt[IP].dst
+        print(f"{ts} {proto:8} {src} -> {dst}")
